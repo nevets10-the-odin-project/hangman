b27400896f853bd3bc72ed01555739bf084cdaa8
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-19 19:33:07 -0800

    Add loop to start_game, add take_turn

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
 class Game
   attr_accessor :file_handler, :secret_word
 
+  GUESS_COUNT = 8
+
   def initialize(file_handler, secret_word)
     self.file_handler = file_handler
     self.secret_word = secret_word
@@ -9,6 +11,13 @@ class Game
   def start_game
     words = file_handler.open_file('words.txt')
     secret_word.pick_random_word(words)
-    puts secret_word.secret_word
+
+    GUESS_COUNT.times do |turn_num|
+      take_turn(turn_num)
+    end
+  end
+
+  def take_turn(turn_num)
+    puts "Guess ##{turn_num + 1}"
   end
 end
