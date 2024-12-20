ce6e7774892fec009123f07637a8a8de372e5b23
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-19 18:58:41 -0800

    Add initialize method to Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,11 @@
 class Game
   attr_accessor :file_handler, :secret_word
 
+  def initialize(file_handler, secret_word)
+    self.file_handler = file_handler
+    self.secret_word = secret_word
+  end
+
   def start_game
   end
 end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,8 +2,9 @@ require_relative 'game'
 require_relative 'file_handler'
 require_relative 'secret_word'
 
-file = File_handler.new
+file_handler = File_handler.new
 secret_word = Secret_word.new
+game = Game.new(file_handler, secret_word)
 
-words = file.open_file('words.txt')
+words = file_handler.open_file('words.txt')
 puts secret_word.pick_random_word(words)
