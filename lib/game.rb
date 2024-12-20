d923cdbf0a600d3562817b2b0d9fc92b156c117d
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-19 21:16:12 -0800

    Add current_turn var, refactor turn loop

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,23 @@
 class Game
-  attr_accessor :file_handler, :secret_word
+  attr_accessor :file_handler, :secret_word, :current_turn
 
   GUESS_COUNT = 12
 
   def initialize(file_handler, secret_word)
     self.file_handler = file_handler
     self.secret_word = secret_word
+    self.current_turn = 1
   end
 
   def start_game
     words = file_handler.open_file('words.txt')
     secret_word.pick_random_word(words)
 
-    GUESS_COUNT.times do |turn_num|
-      take_turn(turn_num)
+    while current_turn <= GUESS_COUNT
+      take_turn(current_turn)
       break if secret_word.secret_word.all? { |letter| secret_word.correct_guesses.include?(letter) }
+
+      self.current_turn += 1
     end
 
     puts "The word was #{secret_word.secret_word.join.chomp}!"
@@ -24,7 +27,7 @@ class Game
     system 'clear -x'
     secret_word.print_secret_word
     puts ''
-    puts "Guess ##{turn_num + 1} of #{GUESS_COUNT}"
+    puts "Guess ##{turn_num} of #{GUESS_COUNT}"
     puts "CORRECT: #{secret_word.correct_guesses}"
     puts "WRONG: #{secret_word.incorrect_guesses}"
     puts 'Type save to save the game'
