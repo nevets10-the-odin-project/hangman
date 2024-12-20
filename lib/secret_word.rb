e53b5c0561989e3fc1aadaae18c0e2a1bc08f481
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-19 18:37:12 -0800

    Add secret_word.rb

---
+++ lib/secret_word.rb
@@ -0,0 +1,18 @@
+class Secret_word
+  attr_accessor :secret_word
+
+  def pick_random_word
+  end
+
+  def print_secret_word
+  end
+
+  def check_guess
+  end
+
+  def update_correct
+  end
+
+  def update_incorrect
+  end
+end
